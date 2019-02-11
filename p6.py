add4cd754f09208b24377e0e4305dc7f7187f9c6
peeranon98 <peeranon98@hotmail.com>
2019-02-11 22:13:24 +0700

    Signed-off-by: peeranon98 <peeranon98@hotmail.com>

--- p6.py
+++ p6.py
@@ -6,24 +6,24 @@ arrsize = 1000
 NUM_CPU = 4
 chunksize = int(arrsize/NUM_CPU)
 lock = multiprocessing.Lock()
+glob = multiprocessing.Value('i',0)
 def generate():
     arr = []
     for i in range(arrsize):
         arr.append(random.randint(0,100000))
     return arr
-def findmax(start,stop,A):
+def findmax(start,stop,A,glob):
     for i in range (start,stop):
-        if A[i] > local :
-            local = A[i]
-    glob.append(local)
+        if A[i] > glob.value :
+            with lock:
+                glob.value = A[i]
 if __name__ == '__main__':
     ptr = 0
     A = generate()
-    glob = multiprocessing.Value('i',0)
     proc = []
     st = time.time()
     for i in range (NUM_CPU):
-        p = multiprocessing.Process(target = findmax, args = (ptr,ptr+chunksize,A))
+        p = multiprocessing.Process(target = findmax, args = (ptr,ptr+chunksize,A,glob))
         proc.append(p)
         ptr = ptr+chunksize
     for p in proc:
@@ -31,5 +31,5 @@ if __name__ == '__main__':
     for p in proc:
         p.join()
     et = time.time()
-    print(f"Global maxima : {glob}")
+    print(f"Global maxima : {glob.value}")
     print(f"\nExecution time: {((et-st)*1000)} ms.")
